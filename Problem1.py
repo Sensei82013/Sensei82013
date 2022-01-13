87ac75e405effc9247e881d09f125cf2bd2af59c
Tobias Spitz <spitzt@ecegrid-thin1.ecn.purdue.edu>
2022-01-13 17:00:56 -0500

    done 1

--- Problem1.py
+++ Problem1.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-import math
-number = 100
+import math as m
+n = 100
+
 # Your code should be below this line
+if n > 0 and n % 2 == 0 and (m.sqrt(5*(n**2) + 4)%2==0 or m.sqrt(5*(n**2) - 4) % 2 == 0 or m.sqrt(5*(n**2) + 4)%3==0 or m.sqrt(5*(n**2) - 4) % 3 == 0):
+
+    print("Yes")
+else:
+    print("No")
+
+#If your input is number = 2 or 34 or 144, the output should be "Yes".
+#If your input if number = 4 or 1 or 5 or -8, the output should be "No". Note that 1 and 5 in this example are part of the 
--- Problem2.py
+++ Problem2.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 import math
-n = 21
+n = 24
+sat = 0
+sun = 1
 # Your code should be below this line
+if n < 1 or n > 31:
 
+    print("Not valid")
+
+elif ((n - 1) % 7) == sat or ((n - 1) % 7) == sun:
+    print("Weekend")
+
+else:
+    print("Weekday")
